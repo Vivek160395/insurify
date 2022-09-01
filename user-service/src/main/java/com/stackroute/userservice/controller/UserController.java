bd63fe8f639022007f300be5c74ae9b9c7531b62
Shradha <P@ssw0rd>
2022-09-01 14:44:50 +0530

    created variable of user service in user controller

--- user-service/src/main/java/com/stackroute/userservice/controller/UserController.java
+++ user-service/src/main/java/com/stackroute/userservice/controller/UserController.java
@@ -1,4 +1,16 @@
 package com.stackroute.userservice.controller;
 
+import com.stackroute.userservice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 }
