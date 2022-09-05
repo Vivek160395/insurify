5d1b7e02e3e49d1ec696203155ac014864f7e1d8
vivekkrishna005@gmail.com <vivekkrishna005@gmail.com>
2022-09-05 11:50:25 +0530

    updated

--- insurance-service/src/main/java/com/stackroute/insuranceservice/model/HealthInsurancePolicy.java
+++ insurance-service/src/main/java/com/stackroute/insuranceservice/model/HealthInsurancePolicy.java
@@ -4,22 +4,23 @@ import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 
-<<<<<<< HEAD
+import java.util.List;
+
 @Data
-@Document(indexName = "insurancepolicy")
-=======
-@Document(indexName = "InsurancePolicy")
-@Getter
-@Setter
 @NoArgsConstructor
-@AllArgsConstructor
->>>>>>> 80029c5fa4a7b2fc8abc6d214307ff5a27c3e005
-public class InsurancePolicy {
+@Document(indexName = "insurance-policy")
+public class HealthInsurancePolicy {
 
     @Id
     Integer policyId;
     String policyName;
     String policyDescription;
-    String policyDuration;
     String insuranceType;
+    String policyBenefits;
+    String policyDocuments;
+    List<String> sumsInsured;
+    List<String> premium;
+    List<String> policyDuration;
+    List<String> addOns;
+    List<String> addOnsPremium;
 }
