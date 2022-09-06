0f0f459abfad2ce7f3139f6b1ff722cf2fe5a5b1
Ritvik Gupta <gritvik97@gmail.com>
2022-09-06 21:11:03 +0530

    added user in model class and added user repository

--- recommendation-service/src/main/java/com/stackroute/recommendationservice/repository/Insurance_Repository.java
+++ recommendation-service/src/main/java/com/stackroute/recommendationservice/repository/Insurance_Repository.java
@@ -16,17 +16,11 @@ public interface Insurance_Repository extends Neo4jRepository<Insurance,Integer>
     @Query("MATCH (a:Insurance{insuranceId:$insuranceId}),(b:Occupation{occupationName: $occupationName}) CREATE (a)-[:for]->(b)")
     void createOccupationRelation(int insuranceId,String occupationName);
 
-//    @Query("MATCH (a:Insurance{insuranceId:$insuranceId}),(b:Vehicle{vehicleType: $vehicleType}) CREATE (a)-[:for]->(b)")
-//    void createVehicleRelation(int insuranceId,String vehicleType);
-
     @Query("MATCH (a:Insurance{insuranceId:$insuranceId}),(b:InsuranceType{insuranceType:$insuranceType}) RETURN exists ((a)<-[:TypeOfInsurance]-(b))")
     boolean checkInsuranceTypeRelationship(int insuranceId,String insuranceType);
 
     @Query("MATCH (a:Insurance{insuranceId:$insuranceId}),(b:Age{age:$age}) RETURN exists ((a)<-[:ForAge]-(b))")
     boolean checkAgeRelationship(int insuranceId,int age);
-
-//    @Query("MATCH (a:Insurance{insuranceId:$insuranceId}),(b:Vehicle{vehicleType:$vehicleType}) RETURN exists ((a)<-[:ForAge]-(b))")
-//    boolean checkVehicleRelationship(int insuranceId,String vehicleType);
 
     @Query("MATCH (a:Insurance{insuranceId:$insuranceId}),(b:Occupation{occupationName: $occupationName}) RETURN exists ((a)<-[:for]-(b))")
     boolean checkOccupationRelation(int insuranceId,String occupationName);
@@ -40,6 +34,21 @@ public interface Insurance_Repository extends Neo4jRepository<Insurance,Integer>
     @Query("MATCH (a:Insurance),(b:InsuranceType{insuranceType:$insuranceType}) WHERE (b)<-[:TypeOfInsurance]-(a) RETURN a")
     List<Insurance> getAllInsurancesMatchingWithInsuranceType(String insuranceType);
 
+    @Query("MATCH (n:Insurance) RETURN n")
+    List<Insurance> getAllInsurances();
+
+    @Query("MATCH (a:User),(b:Insurance) WHERE b.insuranceId = $insuranceId AND a.userEmail=$userEmail CREATE (a)-[r:Bought]->(b)")
+    void createUserToInsuranceRelationship(int insuranceId,String userEmail);
+
+    @Query("MATCH (a:User),(b:Insurance) WHERE b.insuranceId = $insuranceId AND a.userEmail=$userEmail RETURN exists ((a)<-[:Bought]-(b))")
+    boolean checkUserToInsuranceRelationship(int insuranceId,String userEmail);
+}
+
+//    @Query("MATCH (a:Insurance{insuranceId:$insuranceId}),(b:Vehicle{vehicleType:$vehicleType}) RETURN exists ((a)<-[:ForAge]-(b))")
+//    boolean checkVehicleRelationship(int insuranceId,String vehicleType);
+
 //    @Query("MATCH (a:Insurance),(b:Vehicle{vehicleType:$vehicleType}) WHERE (b)<-[:TypeOfInsurance]-(a) RETURN a")
 //    List<Insurance> getAllInsurancesMatchingWithVehicle(String vehicleType);
-}
+
+//    @Query("MATCH (a:Insurance{insuranceId:$insuranceId}),(b:Vehicle{vehicleType: $vehicleType}) CREATE (a)-[:for]->(b)")
+//    void createVehicleRelation(int insuranceId,String vehicleType);
