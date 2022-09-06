0823bfbbf8faae1fc255b1948ffa3759711edcea
unknown <srishareddy77@gmail.com>
2022-09-06 13:32:23 +0530

    done

--- product-webapp/app/src/app/app-routing.module.ts
+++ product-webapp/app/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RecommendationComponent } from './recommendation/recommendation.component';
 
-const routes: Routes = [{path:"",component:RecommendationComponent}];
+const routes: Routes = [{path:"recommendation",component:RecommendationComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
