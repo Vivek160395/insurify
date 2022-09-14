cccbbc640592aa0f1edc802a9b55fa63483a64e6
mufees 1.2 <mufeespasha786@gamil.com>
2022-09-14 12:59:31 +0530

    abc

---
+++ product-webapp/app/src/app/userservice1.service.ts
@@ -0,0 +1,12 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Userservice1Service {
+
+  constructor(private httpclient:HttpClient) { }
+
+  
+}
