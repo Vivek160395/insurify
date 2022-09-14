be653b3e303136f13fda2f2a7f14137bcfc1b561
unknown <Ajay Kumar>
2022-09-14 18:05:24 +0530

    updated user service

--- user-service/src/main/java/com/stackroute/userservice/controller/UserController.java
+++ user-service/src/main/java/com/stackroute/userservice/controller/UserController.java
@@ -47,11 +47,11 @@ public class UserController {
     }
 
     @PutMapping("/updateUser/{emailId}")
-    public ResponseEntity<?> updateUserInfo(@RequestParam("userDetails") User user, @PathVariable String emailId, @RequestParam("imageFile") MultipartFile file) throws UserNotRegisteredException, IOException {
+    public ResponseEntity<?> updateUserInfo(@RequestParam("userDetails") String user, @PathVariable String emailId, @RequestParam("imageFile") MultipartFile file) throws UserNotRegisteredException, IOException {
         try {
-//            User user1 = new ObjectMapper().readValue(user,User.class);
+            User user1 = new ObjectMapper().readValue(user,User.class);
 
-            return new ResponseEntity<>(userService.updateUser(user, emailId, file), HttpStatus.OK);
+            return new ResponseEntity<>(userService.updateUser(user1, emailId, file), HttpStatus.OK);
         } catch (UserNotRegisteredException | IOException e) {
             e.getMessage();
             throw e;
