0cbc00a2b649fe4bbe091f5011995140e27fa287
unknown <Ajay Kumar>
2022-09-19 15:42:47 +0530

    modified user service

--- product-webapp/app/src/app/update/update.component.ts
+++ product-webapp/app/src/app/update/update.component.ts
@@ -30,8 +30,8 @@ export class UpdateComponent implements OnInit {
   edit:boolean=true;
   view:boolean=false;
 
-  address:Address=new Address("","","","","","");
-  user1:User=new User("","","","","","","","",this.address,"","");
+  // address:Address=new Address("","","","","","");
+  // user1:User=new User("","","","","","","","",this.address,"","");
 
  
 
@@ -99,20 +99,20 @@ profile=new FormGroup({
     this.edit=true;
     this.view=false;
     console.log(this.profile.value.name);
-    this.user1.name=this.profile.value.name;
-    this.user1.dateOfBirth=this.profile.value.dob;
-    this.user1.gender=this.profile.value.gender;
-    this.user1.mobileNo=this.profile.value.mobile;
-    this.user1.aadharNo=this.profile.value.aadhar;
-    this.user1.panNo=this.profile.value.pan;
-    this.user1.address.city=this.profile.value.hno;
-    this.user1.address.landmark=this.profile.value.landmark;
-    this.user1.address.city=this.profile.value.city;
-    this.user1.address.street=this.profile.value.street;
-    this.user1.address.state=this.profile.value.state;
-    this.user1.address.pinCode=this.profile.value.pincode;
-    console.log(this.user1.address);
-    this.service.updateUserDetails(this.user1);
+    // this.user1.name=this.profile.value.name;
+    // this.user1.dateOfBirth=this.profile.value.dob;
+    // this.user1.gender=this.profile.value.gender;
+    // this.user1.mobileNo=this.profile.value.mobile;
+    // this.user1.aadharNo=this.profile.value.aadhar;
+    // this.user1.panNo=this.profile.value.pan;
+    // this.user1.address.city=this.profile.value.hno;
+    // this.user1.address.landmark=this.profile.value.landmark;
+    // this.user1.address.city=this.profile.value.city;
+    // this.user1.address.street=this.profile.value.street;
+    // this.user1.address.state=this.profile.value.state;
+    // this.user1.address.pinCode=this.profile.value.pincode;
+    // console.log(this.user1.address);
+    // this.service.updateUserDetails(this.user1);
 }
 
 onEdit(){
