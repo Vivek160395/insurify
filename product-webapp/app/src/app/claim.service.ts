e35a0cbbf38d1172d7a10030a68b6a582a8ef8db
laxmisreenuvasan@gmail.com <laxmisreenuvasan@gmail.com>
2022-09-26 18:09:07 +0530

    update

--- product-webapp/app/src/app/claim.service.ts
+++ product-webapp/app/src/app/claim.service.ts
@@ -20,7 +20,7 @@ export class ClaimService {
 
  
 
-  customerPolicyId:string="12345"
+  customerPolicyId:string="123456"
   baseurl:any="http://localhost:8084/api/";
   getUserDetails():Observable<any>{
     return this.http.get(this.baseurl+"get/"+this.customerPolicyId);
--- product-webapp/app/src/app/claim/claim.component.ts
+++ product-webapp/app/src/app/claim/claim.component.ts
@@ -72,16 +72,15 @@ export class ClaimComponent implements OnInit {
     }
   }
   claimForm = new FormGroup({
-    document: new FormControl("",),
-    describeEventauto: new FormControl("",),
-    describeEventhealth: new FormControl("",),
-    describeEventlife: new FormControl("",),
+    // document: new FormControl("",),
+    description: new FormControl("",),
+   
     claimType: new FormControl("",),
-    claimAuto: new FormControl("",),
+    
     claimAmount: new FormControl("",),
-    claimHealth: new FormControl("",),
+   
     claimDate: new FormControl("",),
-    eventDate: new FormControl("",),
+    claimSubmissionDate: new FormControl("",),
     customerPolicyId: new FormControl(localStorage.getItem('customerPolicyId')),
     insurancePolicyId: new FormControl(localStorage.getItem('policyId')),
     email:new FormControl(localStorage.getItem('email'))
