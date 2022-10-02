ab8b5d8a77cf271c8eb05b7d9fe0fd94c58da5eb
unknown <srishareddy77@gmail.com>
2022-10-02 21:33:47 +0530

    done

--- product-webapp/app/src/app/chat/chat.component.ts
+++ product-webapp/app/src/app/chat/chat.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import * as Stomp from 'stompjs';
 import * as SockJS from 'sockjs-client';
 import { HttpClient } from '@angular/common/http';
@@ -13,20 +13,17 @@ import { UserService } from '../user.service';
 export class ChatComponent implements OnInit {
 
 
+  @ViewChild('endOfChat')
+  endOfChat!: ElementRef;
+
   role:string="user";
   loginId:any='john@gmail.com';
   otherId:any=null;
   
   names:any[]=[];
-  msgs:any[]=[];
   showMsgs:boolean=true;
+  disable:boolean=true;
 
-  // userId:string='john@gmail.com';
-  // advisorId:any=null;
-  // chaturl:any=this.userId+"&"+this.advisorId;
-  // myId:any=this.userId;
-  
-  
   msg:any[]=[
     {
     "id":null,
@@ -40,71 +37,97 @@ export class ChatComponent implements OnInit {
     "msg":null
   }
 
+  registerMsg:any={
+      "chatRoomName":"null",
+      "userName":null,
+      "advisorName":null
+  }
+
 constructor(private http:HttpClient,private service:UserService) { }
 
 ngOnInit(): void {
-   
     this.getnames();
    }
 
-// registerOnConnect(){
-//   this.service.registerChatRoom(this.msg).subscribe();
-// }
+   connect(id:any){
+    if(this.role=="user"){
+      this.registerMsg.userName=this.loginId;
+      this.registerMsg.advisorName=id;
+      this.registerMsg.chatRoomName=this.loginId+"&"+id;
+      this.service.registerChatRoom(this.registerMsg).subscribe(
+        data=>{
+          this.getnames();
+          this.getMsgsByName(id);
+        }
+      );
+    }
+    else{
+      this.registerMsg.userName=id;
+      this.registerMsg.advisorName=this.loginId;
+      this.registerMsg.chatRoomName=id+"&"+this.loginId;
+      this.service.registerChatRoom(this.registerMsg).subscribe(
+        data=>{
+          this.getnames();
+          this.getMsgsByName(id);
+        }
+      );
+ }
+     
+   }
 
 getnames(){
-  this.service.getNames(this.loginId).subscribe(
-    (data)=>{
+  this.service.getNames(this.loginId).subscribe((data)=>{
   for(let i:number=0;i<data.length;i++){
     if(this.role=="user"){
       this.names[i]=data[i].advisorName;
     }
     else{
       this.names[i]=data[i].userName
-    }
-   }
-});
-}
+    }}});}
 
 getMsgsByName(id:string){
   this.showMsgs=false;
   this.otherId=id;
     if(this.role=='user'){
-      this.service.getMsgs(this.loginId+"&"+this.otherId).subscribe(
-        data=>{
-          console.log(data);
-          this.msgs=data;
+      this.service.getMsgs(this.loginId+"&"+this.otherId).subscribe(data=>{
           this.msg=data;
-          console.log(this.msg);
           for(let i:number=0;i<data.length;i++){
             if(data[i].id==this.loginId){
               this.msg[i].show=true;
-            }
-          }
+            }} }) 
         }
-      )
-    }
     else{
-      this.service.getMsgs(this.otherId+"&"+this.loginId).subscribe(
-        data=>{
-          console.log(data);
-          this.msgs=data;
-        }
-      );
+      this.service.getMsgs(this.otherId+"&"+this.loginId).subscribe( data=>{
+          this.msg=data;
+          for(let i:number=0;i<data.length;i++){
+            if(data[i].id==this.loginId){
+              this.msg[i].show=true;}}});
     }
-    
-}
+    this.scrollToBottom();
+  }
 
 sendMessage(){
-if(this.role=="user"){
-    this.service.updateMsgList(this.chatMsg,this.loginId+"&"+this.otherId).subscribe();
-}
-  else{
-    this.service.updateMsgList(this.chatMsg,this.otherId+"&"+this.loginId).subscribe();
-}
-this.chatMsg.msg=null;
-this.getMsgsByName(this.otherId);
 
-  }
+if(this.chatMsg.msg!=null){
+if(this.role=="user"){ this.service.updateMsgList(this.chatMsg,this.loginId+"&"+this.otherId).subscribe(data=>{
+        this.getMsgsByName(this.otherId);} );
+     }
+  else{ this.service.updateMsgList(this.chatMsg,this.otherId+"&"+this.loginId).subscribe( data=>{
+        this.getMsgsByName(this.otherId) });
+    }
+this.chatMsg.msg=null;
+ }
+}
   
+scrollToBottom(){
+  setTimeout(()=>{
+    if(this.endOfChat){
+      this.endOfChat.nativeElement.scrollIntoView({behavior:"smooth"
+      })
+    }
+
+  },100)
+ 
+}
 }
 
