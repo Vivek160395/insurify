91665303de448869f94e4df41128a1912704d5d0
Chaitali <chaitali.chatterjee@stackroute.in>
2022-10-09 10:03:29 +0530

    static folder is created

--- product-webapp/app/src/app/Services/recommendation-service.service.ts
+++ product-webapp/app/src/app/Services/recommendation-service.service.ts
@@ -11,7 +11,7 @@ export class RecommendationServiceService {
   constructor(private http: HttpClient) { }
   // basicUrl = "http://localhost:8080/recommendation/Recommendation";
   // policyUrl = "http://localhost:8080/insurance/api/vk1/";
-  basicUrl='https://insurify.stackroute.io/recommendation/Recommendation'
+  basicUrl='https://insurify.stackroute.io/recommendation/Recommendation/'
   policyUrl='https://insurify.stackroute.io/insurance/api/vk1/'
   basicurl='https://insurify.stackroute.io/'
   getAllInsurances(): Observable<any> {
